2683cd81dccb7d9cf8d04184bd0e630aeb427410
Michael Portuesi <mportuesisf@gmail.com>
2010-11-21 14:30:41 -0800

    Implement test cases for Telescope model obj

--- src/com/jotabout/eyepiececalc/model/TelescopeTest.java
+++ src/com/jotabout/eyepiececalc/model/TelescopeTest.java
@@ -1,64 +1,59 @@
 package com.jotabout.eyepiececalc.model;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TelescopeTest {
+	
+	private Telescope mTelescope;
 
 	@Before
 	public void setUp() throws Exception {
+		mTelescope = new Telescope();
 	}
 
 	@Test
 	public void testTelescope() {
-		fail("Not yet implemented");
+		assertThat( mTelescope, hasProperty("id") );
+		assertThat( mTelescope, hasProperty("name") );
+		assertThat( mTelescope, hasProperty("focalLength") );
+		assertThat( mTelescope, hasProperty("aperture") );
 	}
 
 	@Test
 	public void testTelescopeLongStringIntInt() {
-		fail("Not yet implemented");
+		mTelescope = new Telescope( 89, "Meade LX-10", 2000, 203 );
+		assertThat( mTelescope.getId(), equalTo(89L) );
+		assertThat( mTelescope.getName(), equalTo("Meade LX-10") );
+		assertThat( mTelescope.getFocalLength(), equalTo( 2000 ) );
+		assertThat( mTelescope.getAperture(), equalTo( 203 ) );
 	}
 
 	@Test
-	public void testGetId() {
-		fail("Not yet implemented");
+	public void testGetSetId() {
+		mTelescope.setId( 1522 );
+		assertThat( mTelescope.getId(), equalTo( 1522L ));
 	}
 
 	@Test
-	public void testSetId() {
-		fail("Not yet implemented");
+	public void testGetSetName() {
+		mTelescope.setName( "Celestron NexStar" );
+		assertThat( mTelescope.getName(), equalTo( "Celestron NexStar" ) ); 
 	}
 
 	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
+	public void testGetSetFocalLength() {
+		mTelescope.setFocalLength( 1875 );
+		assertThat( mTelescope.getFocalLength(), equalTo( 1875 ) );
 	}
 
 	@Test
-	public void testSetName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFocalLength() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetFocalLength() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetAperture() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetAperture() {
-		fail("Not yet implemented");
+	public void testGetSetAperture() {
+		mTelescope.setAperture( 380 );
+		assertThat( mTelescope.getAperture(), equalTo( 380 ) );
 	}
 
 }
